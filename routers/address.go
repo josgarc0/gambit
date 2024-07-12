bab3d8ebc5f2034592420502f1c4939ecda5c271
josgarc0 <jgarciaguti0@gmail.com>
2024-07-12 18:14:02 -0500

    Ultimo Commit

--- routers/address.go
+++ routers/address.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"github.com/josgarc0/gambit/bd"
 	"github.com/josgarc0/gambit/models"
@@ -44,4 +45,34 @@ func InsertAddress(body string, User string) (int, string) {
 	}
 
 	return 200, "InsertAddress Ok "
+}
+
+func UpdateAddress(body string, User string, id int) (int, string) {
+	var t models.Address
+
+	err := json.Unmarshal([]byte(body), &t)
+	if err != nil {
+		return 400, "Error en los datos recibidos " + err.Error()
+	}
+
+	isAdmin, msg := bd.UserIsAdmin(User)
+	if !isAdmin {
+		return 400, msg
+	}
+	t.AddId = id
+
+	var encontrado bool
+	err, encontrado = bd.AddressExists(User, t.AddId)
+	if !encontrado {
+		if err != nil {
+			return 400, "Error al intentar buscar Address para el usuario " + User + " > " + err.Error()
+		}
+		return 400, "no se encuentra un registro de ID de Usuario asociado a esa ID de Address"
+	}
+
+	err2 := bd.UpdateAddress(t)
+	if err2 != nil {
+		return 400, "Ocurrio un error al intentar realizar el UPDATE del address " + strconv.Itoa(id) + " > " + err2.Error()
+	}
+	return 200, "UpdateAddress Ok"
 }
