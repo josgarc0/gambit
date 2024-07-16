26023e8a8216029f7b739506e7460842b42b8de7
josgarc0 <jgarciaguti0@gmail.com>
2024-07-15 21:19:27 -0500

    Ultimo Commit

--- bd/order .go
+++ bd/order .go
@@ -35,7 +35,7 @@ func InsertOrder(o models.Orders) (int64, error) {
 		return 0, err2
 	}
 	for _, od := range o.OrdersDetails {
-		sentencia = "INSERT INTO order_detail (OD_OrderId, OD_ProdId, OD_Quantity, OD_Price) VALUES (" + strconv.Itoa(int(LastInsertId))
+		sentencia = "INSERT INTO orders_detail (OD_OrderId, OD_ProdId, OD_Quantity, OD_Price) VALUES (" + strconv.Itoa(int(LastInsertId))
 		sentencia += "," + strconv.Itoa(od.OD_ProdId) + "," + strconv.Itoa(od.OD_Quantity) + "," + strconv.FormatFloat(od.OD_Price, 'f', 1, 64) + ")"
 		fmt.Println(sentencia)
 		_, err = Db.Exec(sentencia)
