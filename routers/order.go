39237d4e0da4069734b621ea69184c72515220e2
josgarc0 <jgarciaguti0@gmail.com>
2024-07-15 21:01:26 -0500

    Ultimo Commit

--- routers/order.go
+++ routers/order.go
@@ -12,6 +12,7 @@ import (
 func InsertOrder(body string, User string) (int, string) {
 	var o models.Orders
 	err := json.Unmarshal([]byte(body), &o)
+	println(o)
 	if err != nil {
 		return 400, "Error en los datos recibidos " + err.Error()
 	}
