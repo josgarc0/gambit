cdaeb079b1faf3ea2af1c92e91f36646959cd5c6
josgarc0 <jgarciaguti0@gmail.com>
2024-07-15 20:36:21 -0500

    Ultimo Commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -140,5 +140,9 @@ func ProcesoAddress(body string, path string, method string, user string, id int
 }
 
 func ProcesoOrder(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+	switch method {
+	case "POST":
+		return routers.InsertOrder(body, user)
+	}
 	return 400, "Methos invalid"
 }
---
+++ routers/order.go
@@ -0,0 +1,49 @@
+package routers
+
+import (
+	"encoding/json"
+	"strconv"
+
+	//"github.com/aws/aws-lambda-go/events"
+	"github.com/josgarc0/gambit/bd"
+	"github.com/josgarc0/gambit/models"
+)
+
+func InsertOrder(body string, User string) (int, string) {
+	var o models.Orders
+	err := json.Unmarshal([]byte(body), &o)
+	if err != nil {
+		return 400, "Error en los datos recibidos " + err.Error()
+	}
+	o.Order_UserUUID = User
+	OK, message := ValidaOrder(o)
+	if !OK {
+		return 400, message
+	}
+	result, err2 := bd.InsertOrder(o)
+	if err2 != nil {
+		return 400, "Ocurrió un error al intentar realizar el registro de la orden " + err2.Error()
+	}
+	return 200, "{ OrderID : " + strconv.Itoa(int(result)) + "}"
+
+}
+
+func ValidaOrder(o models.Orders) (bool, string) {
+	if o.Order_Total == 0 {
+		return false, "Debe indicar el total de la orden"
+	}
+	count := 0
+	for _, od := range o.OrdersDetails {
+		if od.OD_ProdId == 0 {
+			return false, "Debe indicar el ID del producto en el detalle de la Orden"
+		}
+		if od.OD_Quantity == 0 {
+			return false, "Debe indicar la cantidad del producto en el detalle de la Orden"
+		}
+		count++
+	}
+	if count == 0 {
+		return false, "Debe indicar items en la orden"
+	}
+	return true, ""
+}
